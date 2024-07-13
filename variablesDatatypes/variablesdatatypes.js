53c66694806218d3f0bf7365a8dc9f7e4933ebf9
Divya <divyamaurya025@gmail.com>
2024-07-13 20:51:49 +0530

    Day1: Variable and Data types

--- variablesDatatypes/variablesdatatypes.js
+++ variablesDatatypes/variablesdatatypes.js
@@ -1,1 +1,33 @@
-console.log("Divya Maurya");
+console.log("Variable Declaration");
+console.log("Activity 1");
+var numb = 2;
+console.log("number declared with var", numb);
+let str = "divya";
+console.log("string declared with let", str);
+console.log("************************************************");
+console.log("Activity 2");
+const bool = true;
+console.log("booleab declared with const", bool);
+console.log("************************************************");
+let dt_number = 3;
+let dt_string = "Diyva";
+let dt_boolean = true;
+let dt_object = {
+  name: "Divya",
+  age: "23",
+};
+let dt_arr = ["Divya", 2, 5];
+console.log("dt_number", typeof dt_number);
+console.log("dt_string", typeof dt_string);
+console.log("dt_boolean", typeof dt_boolean);
+console.log("dt_object", typeof dt_object);
+console.log("dt_arr", typeof dt_arr);
+console.log("************************************************");
+let firstName = "Nimmi";
+console.log(firstName);
+firstName = "Divya";
+console.log(firstName);
+console.log("************************************************");
+const lastName = "Maurya";
+console.log("Last Name", lastName);
+lastName = "Verma";
