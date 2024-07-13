ea899644fd5ba38b031bea3988c7d1e9bdc01590
Divya <divyamaurya025@gmail.com>
2024-07-14 00:19:05 +0530

    Day1: Updated code

--- variablesDatatypes/variablesdatatypes.js
+++ variablesDatatypes/variablesdatatypes.js
@@ -1,14 +1,15 @@
 console.log("Variable Declaration");
-console.log("Activity 1");
+console.log("*****Activity 1 - Variable Declaration*********");
 var numb = 2;
 console.log("number declared with var", numb);
+
 let str = "divya";
 console.log("string declared with let", str);
-console.log("************************************************");
-console.log("Activity 2");
+
+console.log("******Activity 2 - Const Declaration***********");
 const bool = true;
 console.log("booleab declared with const", bool);
-console.log("************************************************");
+console.log("******Activity 3 - Data types******************");
 let dt_number = 3;
 let dt_string = "Diyva";
 let dt_boolean = true;
@@ -22,12 +23,12 @@ console.log("dt_string", typeof dt_string);
 console.log("dt_boolean", typeof dt_boolean);
 console.log("dt_object", typeof dt_object);
 console.log("dt_arr", typeof dt_arr);
-console.log("************************************************");
+console.log("********Activity 4 - Reassigning Variables*********");
 let firstName = "Nimmi";
 console.log(firstName);
 firstName = "Divya";
 console.log(firstName);
-console.log("************************************************");
+console.log("********Activity 5 - Understanding const***********");
 const lastName = "Maurya";
 console.log("Last Name", lastName);
 lastName = "Verma";
