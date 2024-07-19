86f89a75b55ff13ff3c5867294eea17c1949f601
Divya <divyamaurya025@gmail.com>
2024-07-19 21:57:48 +0530

    Day7: objects

---
+++ Day_7_Objects/objects.js
@@ -0,0 +1,24 @@
+console.log("*****Objects*****");
+console.log("*****Activity 1 - Object Creation and Access*********");
+console.log("_____Task 1_____");
+const book = {
+  title: "Book Title",
+  author: "Book Author",
+  year: "1998",
+  getInfo: function () {
+    return `${title} by ${author}`;
+  },
+};
+console.log(book);
+
+console.log("_____Task 2_____");
+console.log("title", book.title);
+console.log("author", book.author);
+
+// console.log("*****Activity 2 - Object Methods*********");
+// console.log("_____Task 3_____");
+// console.log("_____Task 4_____");
+// console.log("*****Activity 3 - Nested Objects*********");
+// console.log("_____Task 5_____");
+// console.log("_____Task 6_____");
+// console.log("_____Task 7_____");
